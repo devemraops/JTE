6df315a75ee77a03dc4a4a5a90dd594cbbe361ef
emrah basdemir <emrah.basdemir@icloud.com>
2023-02-06 11:26:50 -0600

    added

--- libraries/php/steps/app_deployment.groovy
+++ libraries/php/steps/app_deployment.groovy
@@ -43,17 +43,15 @@ void call() {
                 container(dockerContainer) {
                     def login = ecrLogin(registryIds: [accountId]).replace('docker','podman')
                     String dockerInfo = dockerLogLevel == 'debug' ? 'podman info --debug' : 'podman version'
-                    String demo = command == 'ls' ? 'pwd' : 'podman version'
                     // echo "${env.TRACE_MESSAGE} Logged into ECR"
                     sh "ls -l > commandResult"
                     result = readFile('commandResult').trim()
                     sh(script: """#!/bin/bash
-                        #set -e +o pipefail;                       
+                        set -e +o pipefail;                       
                         ${login} &&
-                        ${demo} &&
                         podman system prune -a --force &&
                         podman build -t 541906215541.dkr.ecr.us-east-1.amazonaws.com/lut:{env.BUILD_NUMBER} . &&
-                        podman push 541906215541.dkr.ecr.us-east-1.amazonaws.com/lut:{env.BUILD_NUMBER} &&
+                        podman push 541906215541.dkr.ecr.us-east-1.amazonaws.com/lut:{env.BUILD_NUMBER}
                     """, label: 'create image latest')
                     // env.imageDigest = sh(returnStdout: true, script: """#!/bin/bash
                     //     podman image inspect ${env.fullECRRepoName}:${env.versionNumber} -f '{{join.RepoDigest \",\"}}'
