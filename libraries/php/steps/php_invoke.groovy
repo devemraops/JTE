2cd42aad5b7d8bf4f483d640077a2984802f13e3
emrah basdemir <emrah.basdemir@icloud.com>
2023-02-05 18:55:09 -0600

    fixed typo

--- libraries/php/steps/php_invoke.groovy
+++ libraries/php/steps/php_invoke.groovy
@@ -12,8 +12,8 @@ void call() {
     String loglevel = config?.podman?.loglevel ?: 'debug'
     String deployReleaseStage = config?.podman?.deployReleaseStage ?: 'release'
     String deployStage = config?.podman?.deployStage ?: 'deployer'
-    String testStage = config?.podman?.testStage ?= 'tester'
-    String container = config?.podman?.container ?= 'podman'
+    String testStage = config?.podman?.testStage ?: 'tester'
+    String container = config?.podman?.container ?: 'podman'
     String jteVersion = config?.jteVersion ?: '1.0.0'
     String dockerLogLevel = config?.dockerLogLevel?: 'debug' //warn
     String codacyApiToken = config?.codacyApiToken?: 'CODACY_PROJECT_TOKEN'
